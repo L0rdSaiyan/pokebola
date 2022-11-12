2d7626a0fb19374a4d523c7a450b39c531e029a7
joao victor <thehoodgamer20@gmail.com>
2022-11-12 20:41:59 -0300

    feito ajustes finais

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ function abrirPokebola() {
     let pokemon = document.querySelector('#img')
     let classe = document.querySelector('.pokebola')
     let btn = document.querySelector('#botao3')
+    let audio = document.querySelector('#audio')
 
     btn.id = "botaoAbrir"
     classe.className = "pokebolarolar"
@@ -12,23 +13,56 @@ function abrirPokebola() {
 
         topo.id = "abrirTopo"
 
-        let numero = Math.floor((Math.random() * 3) + 1)
+        let numero = Math.floor((Math.random() * 4) + 1)
         console.log(numero)
 
         if (numero === 1) {
 
             pokemon.src = "pokemons/pikachu.png"
-            alertify.message("Você ganhou um Pikachu!")
+            alertify.success("Você ganhou um Pikachu!")
+            audio.src = "audios/pikachu.wav"
+            audio.play()
 
         } else if (numero === 2) {
 
             pokemon.src = "pokemons/squirtle.png"
-            alertify.message("Você ganhou um Squirtle!")
+            alertify.success("Você ganhou um Squirtle!")
+            audio.src = "audios/squirtle.wav"
+            audio.play()
+
+            setTimeout(parar => {
+
+                audio.pause()
+
+            }, 3000)
+
+
+        } else if (numero === 3) {
+
+            pokemon.src = "pokemons/charmander.png"
+            alertify.success("Você ganhou um Charmander!")
+            audio.src = "audios/charmander.wav"
+            audio.play()
+
+            setTimeout(parar => {
+
+                audio.pause()
+
+            }, 3000)
 
         } else {
 
-            pokemon.src = "pokemons/charmander.png"
-            alertify.message("Você ganhou um Charmander!")
+            pokemon.src = "pokemons/bulbasaur.png"
+            alertify.success("Você ganhou um Bulbasaur!")
+            audio.src = "audios/bulbasaur.wav"
+            audio.play()
+
+            setTimeout(parar => {
+
+                audio.pause()
+
+            }, 3000)
+
 
         }
 
@@ -48,3 +82,4 @@ function abrirPokebola() {
 
 
 }
+
