eaca5d9a495777e0663e740dce6c16a32df08bdf
joao victor <thehoodgamer20@gmail.com>
2022-11-12 15:57:46 -0300

    adicionado a pasta de pokemons e aleatoriedade deles aparecerem

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 function abrirPokebola(){
 
     const topo = document.querySelector('#topo')
-    const pikachu = document.querySelector('#img')
+    const pokemon = document.querySelector('#img')
+    topo.id = "topo"
     topo.id = "abrirTopo"
-    pikachu.id = "imgPikachu"
+    pokemon.id = 'img'
+
+    let numero = Math.floor((Math.random()*3)+1)
+    console.log(numero)
+
+    if(numero===1){
+
+        pokemon.src = "pokemons/pikachu.png"
+
+    }else if(numero===2){
+
+        pokemon.src = "pokemons/squirtle.png"
+
+    }else{
+
+        pokemon.src = "pokemons/charmander.png"
+
+    }
+
+    pokemon.id = "imgPokemon"
 
 
 }
