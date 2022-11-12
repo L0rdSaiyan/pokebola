3a5150124b1eb12e297e77f6166de1eafaf51ebc
joao victor <thehoodgamer20@gmail.com>
2022-11-12 15:39:39 -0300

    adiconado uma melhor imagem do pikachu

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 function abrirPokebola(){
 
     const topo = document.querySelector('#topo')
+    const pikachu = document.querySelector('#img')
     topo.id = "abrirTopo"
+    pikachu.id = "imgPikachu"
 
 
 }
