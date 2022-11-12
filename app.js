5b20aa93e1cd7c0a38a8894ded12d29391e1132c
joao victor <thehoodgamer20@gmail.com>
2022-11-12 19:39:43 -0300

    adicionado o Framework AlertifyJS no projeto.

--- app.js
+++ app.js
@@ -1,49 +1,50 @@
-function abrirPokebola(){
+function abrirPokebola() {
 
     let topo = document.querySelector('#topo')
     let pokemon = document.querySelector('#img')
-    let base = document.querySelector('#base')
-    let faixa = document.querySelector('#faixa')
     let classe = document.querySelector('.pokebola')
+    let btn = document.querySelector('#botao3')
 
+    btn.id = "botaoAbrir"
     classe.className = "pokebolarolar"
 
-
     setTimeout(abrir => {
-        
+
         topo.id = "abrirTopo"
 
+        let numero = Math.floor((Math.random() * 3) + 1)
+        console.log(numero)
 
-    let numero = Math.floor((Math.random()*3)+1)
-    console.log(numero)
+        if (numero === 1) {
 
-    if(numero===1){
+            pokemon.src = "pokemons/pikachu.png"
+            alertify.message("Você ganhou um Pikachu!")
 
-        pokemon.src = "pokemons/pikachu.png"
+        } else if (numero === 2) {
 
-    }else if(numero===2){
+            pokemon.src = "pokemons/squirtle.png"
+            alertify.message("Você ganhou um Squirtle!")
 
-        pokemon.src = "pokemons/squirtle.png"
+        } else {
 
-    }else{
+            pokemon.src = "pokemons/charmander.png"
+            alertify.message("Você ganhou um Charmander!")
 
-        pokemon.src = "pokemons/charmander.png"
+        }
 
-    }
+        pokemon.id = "imgPokemon"
 
-    pokemon.id = "imgPokemon"
-    
-    setTimeout(fechar=>{
+        setTimeout(fechar => {
 
-        topo.id = "topo"
-        pokemon.id = "img"
-
-
-    },3000)
+            topo.id = "topo"
+            pokemon.id = "img"
+            btn.id = "botao3"
+            classe.className = "pokebola"
 
 
+        }, 3000)
 
     }, 3000);
 
-    
+
 }
